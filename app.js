bd1a78104cf99a9de4e7df830add79e43ed69300
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-10-05 14:57:44 -0700

    Add helmet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express")
 const cookieParser = require("cookie-parser")
 const logger = require("morgan")
 const cors = require("cors")
+const helmet = require("helmet")
 
 const indexRouter = require("./routes/index")
 const usersRouter = require("./routes/users")
@@ -11,6 +12,7 @@ const BASE_URL = process.env.BASE_URL
 
 const app = express()
 
+app.use(helmet())
 app.use(cors({ origin: BASE_URL }))
 app.use(logger("dev"))
 app.use(express.json())
