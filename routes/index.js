c7b7d2544ce77ab0ba76c01bbadf85917e821cc4
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-10-05 12:50:34 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.sendStatus(200)
+  res.status(200).json({ status: "OK" })
 })
 
 module.exports = router
