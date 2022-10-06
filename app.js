bd9f54a3f83b70811862d35740fc2e0d000d37b3
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-10-05 19:44:54 -0700

    Update CORS
    Rather than saving the URL value, the variable was removed. Since the variable was removed, we were able to access it directly. Also, the value was renamed.

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ const helmet = require("helmet")
 const indexRouter = require("./routes/index")
 const usersRouter = require("./routes/users")
 
-const BASE_URL = process.env.BASE_URL
-
 const app = express()
 
 app.use(helmet())
-app.use(cors({ origin: BASE_URL }))
+app.use(cors({ origin: process.env.ORIGIN }))
 app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
