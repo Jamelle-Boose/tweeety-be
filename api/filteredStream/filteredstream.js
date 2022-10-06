4d62565f11d20452a8ee9108bb8d2bd00bfe8111
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-10-06 16:39:53 -0700

    Update filteredstream.js

--- api/filteredStream/filteredstream.js
+++ api/filteredStream/filteredstream.js
@@ -3,11 +3,21 @@ const { Client } = require("twitter-api-sdk")
 const app = express()
 
 app.get("/", async (req, res) => {
+  const headers = {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  }
+
   try {
+    res.writeHead(200, headers)
+
     const client = new Client(process.env.BEARER_TOKEN)
     const tweets = client.tweets.searchStream()
-
-    for await (const tweet of tweets) console.log(tweet?.data)
+    for await (const tweet of tweets) {
+      // TODO #1 Add a string identifying the type of event described
+      res.write(`data: ${JSON.stringify(tweet)}\n\n`)
+    }
   } catch (e) {
     console.error(e)
   }
