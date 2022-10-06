f41297d974529eb4697db67817d8718f6b6cf8a5
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-10-06 13:41:23 -0700

    Update filteredstream.js

--- api/filteredStream/filteredstream.js
+++ api/filteredStream/filteredstream.js
@@ -1,8 +1,16 @@
 const express = require("express")
+const { Client } = require("twitter-api-sdk")
 const app = express()
 
-app.get("/", (req, res) => {
-  res.sendStatus(200)
+app.get("/", async (req, res) => {
+  try {
+    const client = new Client(process.env.BEARER_TOKEN)
+    const tweets = client.tweets.searchStream()
+
+    for await (const tweet of tweets) console.log(tweet?.data)
+  } catch (e) {
+    console.error(e)
+  }
 })
 
 module.exports = app
