8fc1ace957d6dbaa782328825b84c6c491f490ac
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-10-06 12:45:19 -0700

    add filteredstream route

--- api/index.js
+++ api/index.js
@@ -2,5 +2,6 @@ const express = require("express")
 const router = express.Router()
 
 router.use("/users", require("./users"))
+router.use("/filteredstream", require("./filteredStream"))
 
 module.exports = router
