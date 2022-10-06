f4aae865855d99caa87b3b30de9d8e40efdbb042
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-10-05 20:19:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const logger = require("morgan")
 const cors = require("cors")
 const helmet = require("helmet")
 
-const indexRouter = require("./routes/index")
-const usersRouter = require("./routes/users")
-
 const app = express()
 
 app.use(helmet())
@@ -16,8 +13,8 @@ app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use("/", require("./routes"))
 
-app.use("/", indexRouter)
-app.use("/users", usersRouter)
+app.get("/", (_, res) => res.status(200).json({ status: "OK" }))
 
 module.exports = app
